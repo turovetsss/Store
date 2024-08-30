c5f6fcf9c85e462fbec6766837385cb8723a6ee8
vichklu <119040501+vichklu@users.noreply.github.com>
2024-08-30 14:27:06 +0300

    create project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import styles from '../../styles/Header.module.css'
+import { Link } from 'react-router-dom'
+import {ROUTES} from "../../utils/routes"
+import LOGO from "../../images/logo.svg"
+import AVATAR from "../../images/avatar.jpg"
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <div className={styles.logo}>
+      <Link to={ROUTES.HOME}>
+      <img src={LOGO} alt="Stuff" />
+      </Link>
+      </div>
+
+      <div className={styles.info}>
+        <div className={styles.user}>
+          <div className={styles.avatar} style={{backgroundImage:`url(${AVATAR})`}}></div>
+         <div className={styles.username}>Guest</div>
+        </div>
+        <form className={styles.form}>
+         <div className={styles.icon}>
+          <svg className='icon'>
+            <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#search`}/>
+          </svg>
+         </div>
+         <div className={styles.input}>
+          <input type="search" name="search"
+          placeholder='Search for anything...' 
+          autoComplete='off'
+          onChange={()=>{}}
+          value=""/>
+         </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Route,Routes} from 'react-router-dom'
+import { Home } from "../Home/Home";
+ export const AppRoutes=()=>{
+  return (
+    <Routes>
+      <Route index element={<Home/>}/>
+    </Routes>
+  )
+ }
