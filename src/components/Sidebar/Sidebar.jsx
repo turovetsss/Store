8b5a780cf8cd8df9ade33aa1950c11ce82799f45
vichklu <119040501+vichklu@users.noreply.github.com>
2024-09-02 11:43:50 +0300

    sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import styles from "../../styles/Sidebar.module.css"
 import { NavLink } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 const Sidebar = () => {
+  const {list}=useSelector(({categories})=>categories);
+  console.log("list",list)
   return (
     <section>
     <div className={styles.sidebar}>
@@ -10,11 +13,15 @@ const Sidebar = () => {
       </div>
       <nav>
         <ul className={styles.menu}>
-          <li>
-            <NavLink to={`/categories${1}`}>
-              Link
+          {list.map(({id,name})=>(
+            <li key={id}>
+              <NavLink 
+              className={({isActive})=>`${styles.link} ${isActive ? styles.active:""}`}
+              to={`/categories/${id}`}>
+              {name}
             </NavLink>
             </li>
+          ))}
         </ul>
       </nav>
       
